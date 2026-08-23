08955ffdacee6f7476036bed75c7db018ff58f9e
Edom <edomenaadinew9615@gmail.com>
2026-08-23 07:29:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2089,4 +2089,5 @@ if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
  @app.route('/logo.png')
 def serve_logo():
-    return send_file('file_00000000d93c821094a2e3f7dced7c77.png')
+    return send_file('logo.png')
+
