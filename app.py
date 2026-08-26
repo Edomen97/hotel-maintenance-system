eb09e3b8cd03ad7d4223d923fd5cba1ce466b6d5
Edom <edomenaadinew9615@gmail.com>
2026-08-26 19:03:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3186,4 +3186,4 @@ with app.app_context():
 # RUN
 # --------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=app.config["DEBUG"], host="0.0.0.0", port=5000)
+    app.run(debug=app.config["DEBUG"], host="0.0.0.0", port=5000) 
